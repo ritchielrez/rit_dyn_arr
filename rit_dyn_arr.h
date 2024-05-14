214125e02cb11cbd60c6d7b690900e2be9b0993c
RitchielRez <ritchielrez@gmail.com>
2024-05-14 13:40:52 +0600

    feat: add `rit_dyn_arr_append()`

--- rit_dyn_arr.h
+++ rit_dyn_arr.h
@@ -91,6 +91,7 @@ inline void rit_dyn_arr_realloc(const char *t_file, int t_line,
   }
 }
 
+/// @internal
 inline bool rit_dyn_arr_index_bounds_check(const char *t_file, int t_line,
                                            void *t_rit_dyn_arr,
                                            size_t t_index) {
@@ -121,8 +122,7 @@ inline void rit_dyn_arr_clear(void *t_rit_dyn_arr) {
 /// its size. In this library this is definied as a no-op function.
 inline void shrink_to_fit(void *t_rit_dyn_arr) { (void)t_rit_dyn_arr; }
 
-/// @brief Allocate a rit_dyn_arr.
-/// Internally this macro calls rit_dyn_alloc().
+/// @brief Initialize a rit_dyn_arr.
 #define rit_dyn_arr(t_type, t_rit_dyn_arr, t_size, t_allocator) \
   t_type *t_rit_dyn_arr = (t_type *)rit_dyn_arr_alloc(          \
       __FILE__, __LINE__, sizeof(t_type), t_size, t_allocator)
@@ -140,14 +140,13 @@ inline void shrink_to_fit(void *t_rit_dyn_arr) { (void)t_rit_dyn_arr; }
 
 /// @param t_rit_dyn_arr Where to copy
 /// @param t_rit_dyn_arr_other What to copy
-#define rit_dyn_arr_copy(t_rit_dyn_arr, t_rit_dyn_arr_other, t_allocator) \
-  rit_dyn_arr_reserve(t_rit_dyn_arr_other,                                \
-                      rit_dyn_arr_capacity(t_rit_dyn_arr), t_allocator); \
-  rit_dyn_arr_get_metadata(t_rit_dyn_arr_other)->m_size =                \
-      rit_dyn_arr_size(t_rit_dyn_arr);                                   \
-  for (size_t i = 0; i < rit_dyn_arr_size(t_rit_dyn_arr); i++) {         \
-    rit_dyn_arr_set(t_rit_dyn_arr_other, i,                              \
-                    rit_dyn_arr_at(t_rit_dyn_arr, i));                   \
+#define rit_dyn_arr_copy(t_rit_dyn_arr, t_rit_dyn_arr_other, t_allocator)      \
+  rit_dyn_arr_reserve(t_rit_dyn_arr_other,                                     \
+                      rit_dyn_arr_capacity(t_rit_dyn_arr), t_allocator);       \
+  rit_dyn_arr_get_metadata(t_rit_dyn_arr_other)->m_size =                      \
+      rit_dyn_arr_size(t_rit_dyn_arr);                                         \
+  for (size_t i = 0; i < rit_dyn_arr_size(t_rit_dyn_arr); i++) {               \
+    rit_dyn_arr_set(t_rit_dyn_arr_other, i, rit_dyn_arr_at(t_rit_dyn_arr, i)); \
   }
 
 /// @brief Get the pointer to the first element of an array
@@ -162,39 +161,42 @@ inline void shrink_to_fit(void *t_rit_dyn_arr) { (void)t_rit_dyn_arr; }
 #define rit_dyn_arr_back(t_rit_dyn_arr) \
   (t_rit_dyn_arr[rit_dyn_arr_size(t_rit_dyn_arr) - 1])
 
-#define rit_dyn_arr_push_back(t_rit_dyn_arr, t_value, t_allocator)      \
-  if (rit_dyn_arr_capacity(t_rit_dyn_arr) <=                           \
-      rit_dyn_arr_size(t_rit_dyn_arr) + 1) {                           \
-    rit_dyn_arr_reserve(t_rit_dyn_arr,                                  \
-                        (rit_dyn_arr_size(t_rit_dyn_arr) + 1) * 2,     \
-                        t_allocator);                                   \
-  }                                                                     \
-  rit_dyn_arr_get_metadata(t_rit_dyn_arr)->m_size++;                   \
-  rit_dyn_arr_set(t_rit_dyn_arr, rit_dyn_arr_size(t_rit_dyn_arr) - 1, \
-                  t_value);
+#define rit_dyn_arr_push_back(t_rit_dyn_arr, t_value, t_allocator) \
+  if (rit_dyn_arr_capacity(t_rit_dyn_arr) <=                       \
+      rit_dyn_arr_size(t_rit_dyn_arr) + 1) {                       \
+    rit_dyn_arr_reserve(t_rit_dyn_arr,                             \
+                        (rit_dyn_arr_size(t_rit_dyn_arr) + 1) * 2, \
+                        t_allocator);                              \
+  }                                                                \
+  rit_dyn_arr_get_metadata(t_rit_dyn_arr)->m_size++;               \
+  rit_dyn_arr_set(t_rit_dyn_arr, rit_dyn_arr_size(t_rit_dyn_arr) - 1, t_value);
 
 #define rit_dyn_arr_pop_back(t_rit_dyn_arr) \
   rit_dyn_arr_get_metadata(t_rit_dyn_arr)->m_size--
 
-#define rit_dyn_arr_resize(t_rit_dyn_arr, t_count, t_value, t_allocator) \
+#define rit_dyn_arr_append(t_rit_dyn_arr, t_count, t_value, t_allocator) \
   for (int i = 1; i <= t_count; i++) {                                   \
     rit_dyn_arr_push_back(t_rit_dyn_arr, t_value, t_allocator);          \
   }
 
+#define rit_dyn_arr_resize(t_rit_dyn_arr, t_count, t_value, t_allocator) \
+  rit_dyn_arr_clear(t_rit_dyn_arr);                                      \
+  rit_dyn_arr_append(t_rit_dyn_arr, t_count, t_value, t_allocator)
+
 /// @brief Insert a value in the array before t_pos.
-#define rit_dyn_arr_insert(t_rit_dyn_arr, t_pos, t_value, t_allocator)         \
-  rit_dyn_arr_push_back(t_rit_dyn_arr, 0, t_allocator);                        \
-  for (size_t i = rit_dyn_arr_size(t_rit_dyn_arr) - 1; i >= t_pos; i--) {     \
+#define rit_dyn_arr_insert(t_rit_dyn_arr, t_pos, t_value, t_allocator)       \
+  rit_dyn_arr_push_back(t_rit_dyn_arr, 0, t_allocator);                      \
+  for (size_t i = rit_dyn_arr_size(t_rit_dyn_arr) - 1; i >= t_pos; i--) {    \
     rit_dyn_arr_set(t_rit_dyn_arr, i, rit_dyn_arr_at(t_rit_dyn_arr, i - 1)); \
-    rit_dyn_arr_set(t_rit_dyn_arr, i - 1, 0);                                 \
-  }                                                                            \
+    rit_dyn_arr_set(t_rit_dyn_arr, i - 1, 0);                                \
+  }                                                                          \
   rit_dyn_arr_set(t_rit_dyn_arr, t_pos - 1, t_value)
 
 /// @brief Remove the value in the array at t_pos.
-#define rit_dyn_arr_erase(t_rit_dyn_arr, t_pos)                                \
-  for (size_t i = t_pos + 1; i < rit_dyn_arr_size(t_rit_dyn_arr); i++) {      \
+#define rit_dyn_arr_erase(t_rit_dyn_arr, t_pos)                              \
+  for (size_t i = t_pos + 1; i < rit_dyn_arr_size(t_rit_dyn_arr); i++) {     \
     rit_dyn_arr_set(t_rit_dyn_arr, i - 1, rit_dyn_arr_at(t_rit_dyn_arr, i)); \
-  }                                                                            \
+  }                                                                          \
   rit_dyn_arr_pop_back(t_rit_dyn_arr)
 
 #endif  // RIT_DYN_ARR_H_INCLUDED
